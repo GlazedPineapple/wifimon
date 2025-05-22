afc246787db2df648544e7ddf7209b4b79d09096
GlazedPineapple <eokovacs1@gmail.com>
2025-05-22 10:11:03 -0400

    added UV package manager

--- main.py
+++ main.py
@@ -2,10 +2,20 @@
 
 import scapy.all as scapy
 import dhcp
+import socket
 
-packets = scapy.sniff(iface="eth0", count=10)
-print(packets)
+
+""" packets = scapy.sniff(iface="eth0", count=10)
+print(packets) """
+
+
+# Gets avalible network interfaces 
+
+print(socket.if_nameindex())
+
+
+#starts dhcp server
 dhcp.start()
 
 leases = dhcp.leases()
-print(leases)
+#print(leases)
